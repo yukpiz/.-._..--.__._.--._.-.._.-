bc741cba9bc653db5b36668f05079b693a09998d
yukpiz <yukpiz@gmail.com>
2015-04-14 13:12:22 +0900

    Added code.

---
+++ highscore/highscore.cpp
@@ -0,0 +1,24 @@
+/*
+ * [Compile]
+ *   g++ -std=c++11 highscore.cpp -o highscore
+ */
+
+#include <iostream>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+int main(
+		int arg,
+		char **args)
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	string line;
+	cin >> line;
+
+}
+
+
