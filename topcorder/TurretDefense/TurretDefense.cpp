ba2ecaf9ffd30dc2a2d97de5a06ea011b614680e
yukpiz <yukpiz@gmail.com>
2015-04-27 21:55:36 +0900

    Added code.

--- topcorder/TurretDefense/TurretDefense.cpp
+++ topcorder/TurretDefense/TurretDefense.cpp
@@ -17,7 +17,23 @@ int TurretDefense::firstMiss(
 	vector<int> ys,
 	vector<int> times)
 {
-	return 0;
+	int xpoint = 0, ypoint = 0, curtime = 0, buffer = 0, limit = 0;
+	for (int i = 0; i < xs.size(); i++) {
+		int xtarget = xs[i], ytarget = ys[i];
+		limit = times[i] - curtime;
+		int exec = abs(xtarget - xpoint) + abs(ytarget - ypoint);
+		xpoint = xtarget;
+		ypoint = ytarget;
+		buffer = limit - exec < 0 ? 0 : limit - exec;
+		if (limit - exec < 0) { return i; }
+		curtime = times[i];
+// 		cout << "limit:" << limit << "s" << endl;
+// 		cout << "exec:" << exec << "s" << endl;
+// 		cout << "buffer:" << buffer << "s" << endl;
+// 		cout << "curtime:" << curtime << "s" << endl;
+// 		cout << endl;
+	}
+	return -1;
 }
 
 int main(
@@ -25,6 +41,9 @@ int main(
 	char **args)
 {
 	TurretDefense *defense;
-	cout << defense->firstMiss({1}, {2}, {3}) << endl;
+	cout << defense->firstMiss({3, 5, 6}, {7, 5, 6}, {11, 15, 16}) << endl;
+	cout << defense->firstMiss({1,2,3,4,15}, {1,2,3,4,15}, {100,200,300,400,405}) << endl;
+	cout << defense->firstMiss({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,31}) << endl;
+	cout << defense->firstMiss({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32}) << endl;
 	return 0;
 }
