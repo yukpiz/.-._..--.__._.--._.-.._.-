f70da745dfdb79714979d6295e0650b5760326f0
yukpiz <yukpiz@gmail.com>
2015-05-05 23:42:51 +0900

    Updated code.

--- rena/week.cpp
+++ rena/week.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,8 +11,14 @@ int main(
 	cin >> c;
 	cin.ignore();
 
+	vector<int> sum {0, 0, 0, 0, 0, 0, 0};
 	for (int i = 0; i < c; i++) {
 		int m;
 		cin >> m;
+		sum[i % 7] += m;
+	}
+
+	for (int i = 0; i < sum.size(); i++) {
+		cout << sum[i] << endl;
 	}
 }
